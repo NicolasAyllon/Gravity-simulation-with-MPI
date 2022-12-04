2399265c8705684cd4db4514ec1fc6c57abcac30
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-12-04 18:25:16 +0000

    working on Barnes-Hut tree struct, about to begin insert

--- src/quadtree.h
+++ src/quadtree.h
@@ -19,11 +19,45 @@ struct QuadtreeNode {
   QuadtreeNode* q2;       // NW
   QuadtreeNode* q3;       // SW
   QuadtreeNode* q4;       // SE
-  
-  T total_mass;           // Sum of all particle masses in this region
+
+  T total_mass;           // Sum of particle masses in this region
+  int num_particles;      // Number of particles in this region
   Vec2<T> center_of_mass; // Position of the center of mass for this region
   Particle* particle;     // -internal node: nullptr
                           // -leaf node: pointer to particle
+
+  // Construct a quadtree node for the given region, empty with no particles
+  QuadtreeNode(Region r) 
+      : region(r), 
+        q1(nullptr), 
+        q2(nullptr), 
+        q3(nullptr), 
+        q4(nullptr), 
+        total_mass(T()),            // default-construct: 0 for numeric
+        num_particles(0),
+        center_of_mass(Vec2<T>()),  // default-construct: (0,0) for numeric
+        particle(nullptr) {}
+
+    // [?] Add constructor for quadtree node containing particle?
 };
+
+
+template <typename T>
+struct BarnesHutTree {
+  *QuadtreeNode root;
+
+  BarnesHutTree(const Region<T>& region);
+  bool insert(const Particle& p);
+};
+
+template <typename T>
+BarnesHutTree<T>::BarnesHutTree(const Region<T>& region) 
+    : root(new QuadtreeNode(region)) {}
+
+template <typename T>
+bool BarnesHutTree<T>::insert(const Particle& p) {
+  // TODO
+}
+
 
 #endif // _QUADTREE_H
