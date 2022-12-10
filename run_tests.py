953b579cd568ad530a5e6caa965edc0db7c188ca
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-12-10 18:06:27 +0000

    updated tests to call mpirun

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,6 @@
 import subprocess
 
-PROGRAMS = [
-  "nbody"
-]
-
+PROGRAMS = [ "nbody" ]
 # File names without ".txt" extension, for convenient output file naming
 INPUTFILES = [
   # "nb-2-close"    ,
@@ -11,24 +8,27 @@ INPUTFILES = [
   # "nb-100"        ,
   # "nb-100000"     
 ]
-
-# Maybe change to arrays later
+PROCESSES = [ 4 ]
 STEPS = 1000
 THETA = 0.5
 DT_TIMESTEP = 0.005
-
 NUM_TESTS = 1
 
 for program in PROGRAMS:
     for filename in INPUTFILES:
-        for i in range(NUM_TESTS):
-            subprocess.call([
-              "bin/{}".format(program),
-              "-i", "input/{}.txt".format(filename),
-              "-o", "output/{}-{}.txt".format(filename, STEPS),
-              # "-V", # visualization is not used
-              "-s", str(STEPS),
-              "-t", str(THETA),
-              "-d", str(DT_TIMESTEP)
-            ])
-        print("") # newline after each test
+        for n_processes in PROCESSES:
+            for i in range(NUM_TESTS):
+                subprocess.call([
+                  "mpirun",
+                    "-np", str(n_processes), 
+                  "bin/{}".format(program),
+                    "-i", "input/{}.txt".format(filename),
+                    "-o", "output/mpi/{}-{}-np{}.txt".format(filename, 
+                                                            STEPS, 
+                                                            n_processes),
+                    # "-V", # visualization is not used
+                    "-s", str(STEPS),
+                    "-t", str(THETA),
+                    "-d", str(DT_TIMESTEP)
+                ])
+            print("") # newline after each test
