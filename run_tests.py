77ab04c467e1072b2b481f9a3a73947160db3b6c
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-12-11 04:44:58 +0000

    updated tests to print trials on 1 line

--- run_tests.py
+++ run_tests.py
@@ -4,32 +4,43 @@ PROGRAMS = [ "nbody" ]
 # File names without ".txt" extension, for convenient output file naming
 INPUTFILES = [
   # "nb-2-close"    ,
-  "nb-10"         ,
-  # "nb-100"        ,
+  # "nb-10"         ,
+  "nb-100"        ,
   # "nb-100000"     
 ]
-PROCESSES = [ 1, 2, 3, 4, 8, 16 ]
+
+PROCESSES = [ 
+  1, 2, 3, 4, 6, 
+  # 8, 10, 12, 14, 16
+]
+# PROCESSES = [ 1, 2, 3, 4, 6, 8, 10, 12, 14, 16 ]
 STEPS = 1000
-THETA = 0.5
+THETAS = [
+  # 0, 0.25, 
+  0.5, 
+  # 0.75, 1
+]
 DT_TIMESTEP = 0.005
-NUM_TESTS = 1
+NUM_TESTS = 20
 
 for program in PROGRAMS:
     for filename in INPUTFILES:
         for n_processes in PROCESSES:
-            print("{}-{}/{}".format(filename, STEPS, n_processes))
-            for i in range(NUM_TESTS):
-                subprocess.call([
-                  "mpirun",
-                    "-np", str(n_processes), 
-                  "bin/{}".format(program),
-                    "-i", "input/{}.txt".format(filename),
-                    "-o", "output/mpi/{}-{}-np{}.txt".format(filename, 
-                                                            STEPS, 
-                                                            n_processes),
-                    # "-V", # visualization is not used
-                    "-s", str(STEPS),
-                    "-t", str(THETA),
-                    "-d", str(DT_TIMESTEP)
-                ])
-            print("") # newline after each test
+            for theta in THETAS:
+                print("{}-{}/{}".format(filename, STEPS, n_processes))
+                for i in range(NUM_TESTS):
+                    subprocess.call([
+                      "mpirun",
+                        "-np", str(n_processes), 
+                      "bin/{}".format(program),
+                        "-i", "input/{}.txt".format(filename),
+                        "-o", "output/mpi/{}-{}-np{}.txt".format(filename, 
+                                                                STEPS, 
+                                                                n_processes),
+                        # "-V", # visualization is not used
+                        "-s", str(STEPS),
+                        "-t", str(theta),
+                        "-d", str(DT_TIMESTEP)
+                    ])
+                    print(' ', end='', flush=True) # Python 3
+                print('', end='\n', flush=True) # Python 3
